5c4d845cc36a0ebf5778af4b04f9c2293e8e72d1
Kevin <kchau2@umbc.edu>
2025-04-24 16:14:24 -0400

    Adjust how favorites are saved

--- index.js
+++ index.js
@@ -130,16 +130,6 @@ function searchBoxInitialization(searchBox, markers, AdvancedMarkerElement, map)
     });
 
     markers.push(marker);
-
-    // Add event listener to favorite button
-    const favoriteButton1 = document.getElementById("favorite-button-1");
-    if (favoriteButton1) {
-      favoriteButton1.onclick = () => saveFavoriteSearch(place);
-    }
-    const favoriteButton2 = document.getElementById("favorite-button-2");
-    if (favoriteButton2) {
-      favoriteButton2.onclick = () => saveFavoriteSearch(place);
-    }
   });
 }
 
@@ -192,6 +182,8 @@ function getMap(center) {
 
 // --- FAVORITES FUNCTIONS ---
 function saveFavoriteSearch(place) {
+  //if(!place.place_id) return;
+
   let favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
   if (!favorites.some(fav => fav.place_id === place.place_id)) {
     favorites.push(place);
@@ -270,6 +262,13 @@ export async function initMap() {
   originBox.addListener("place_changed", () => {
     routeDisplay.setOrigin(originBox.getPlace());
   });
+  const topFavButton = document.getElementById("favorite-button-1");
+  if (topFavButton) {
+    topFavButton.onclick = () => {
+      let place = originBox.getPlace();
+      if(place !== undefined) saveFavoriteSearch(place);
+    };
+  }
 
   const destInput = document.getElementById("dest-input");
   const destBox = new google.maps.places.Autocomplete(destInput, {
@@ -279,6 +278,13 @@ export async function initMap() {
   destBox.addListener("place_changed", () => {
     routeDisplay.setDest(destBox.getPlace());
   });
+  const bottomFavButton = document.getElementById("favorite-button-2");
+  if (bottomFavButton) {
+    bottomFavButton.onclick = () => {
+      let place = destBox.getPlace();
+      if(place !== undefined) saveFavoriteSearch(place);
+    };
+  }
 
   map.addListener("bounds_changed", () => {
     originBox.bindTo("bounds", map);
@@ -302,6 +308,10 @@ export async function initMap() {
   navButtonInitialization(map, allMarks);
 
   displayFavoriteSearches(); // Display saved favorites on load
+
+  //hide favorites on load
+  const favs = document.getElementById("favorites-container");
+  favs.style.display = "none";
 }
 
 const loader = new Loader({
